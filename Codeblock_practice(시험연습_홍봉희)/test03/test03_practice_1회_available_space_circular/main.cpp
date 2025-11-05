a94bea5fa08fdc415584d3ea5b7cc1deba58c0b0
pjjpjj111_Desktop <pjjpjj111@naver.com>
2025-11-06 03:49:31 +0900

    practice

--- Codeblock_practice(시험연습_홍봉희)/test03/test03_practice_1회_available_space_circular/main.cpp
+++ Codeblock_practice(시험연습_홍봉희)/test03/test03_practice_1회_available_space_circular/main.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
+
+
 using namespace std;
+
+class Node{
+private:
+    friend class Circularlist;
+    int data;
+    Node* link;
+public:
+    Node(){}
+    Node(int element){
+
+    }
+};
+
+
+
+
+class Circularlist{
+private:
+
+public:
+
+};
+
+
